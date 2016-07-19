628709d06518cfa6961295b9ae94467389a265cd
Sébastien Saunier <seb@saunier.me>
2016-07-19 08:10:12 +0100

    Chuck Norris -> GitHub API

--- api_demo.rb
+++ api_demo.rb
@@ -1,5 +1,5 @@
 require 'json'
 require 'open-uri'
 
-# TODO - Let's get Chuck Norris random jokes through an API
-api_url = 'http://api.icndb.com/jokes/random?escape=javascript'
+# TODO - Let's fetch name and bio from a given GitHub username
+url = url = 'https://api.github.com/users/ssaunier'
